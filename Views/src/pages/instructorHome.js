92650063b22bd75c27e25dcf80a51dc342b1cf24
DulamiA <119730845+DulamiA@users.noreply.github.com>
2024-02-18 19:47:34 +0530

    TM-40 - Student Home and resolving conflicts

--- Views/src/pages/instructorHome.js
+++ Views/src/pages/instructorHome.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './css/insHome.css'; // Import your CSS file
-import './css/stdHome.css'; 
 import logo from './img/logo.jpg'; // Update the path as needed
 import homeImage from './img/home.jpg';
-import courseImage from './img/maths.png';
+import maths from './img/maths.png';
+import science from './img/science.jpg';
+import english from './img/english.jpeg';
 
 const InstructorHome = () => {
   return (
@@ -42,36 +43,87 @@ const InstructorHome = () => {
         </div>
       </div>
 
-      <section className="container content-section" id='content-section'>
-        <div className="items" id='items'>
-          <div className="item-ins" id='item'>
-            <img className="item-image" src={courseImage} alt="" id='item-image'/>
-            <span className="item-title" id='item-title'>Mathematics</span>
-            <div className="item-details" id='item-details'>
-              <p id='grade'>Grade: 9 <br /> Instructor: James Wicked </p>
+      <section className="container content-section">
+        <div className="items">
+          <div className="item-ins">
+            <img className="item-image-ins" src={maths} alt="" />
+            <span className="item-title">Mathematics</span>
+            <div className="item-details">
+              <p>Grade: 10 <br /> Instructor: Jane Kate </p>
+              <p>BSc University of Colombo</p>
+              <div className='star-rating'>
+                <div class='rating'>
+                  <input type="radio" name='rating1' id="rating1"></input>
+                  <label for="rating1" ></label>
+                  <input type="radio" name='rating1' id="rating2"></input>
+                  <label for="rating2"></label>
+                  <input type="radio" name='rating1' id="rating3"></input>
+                  <label for="rating3"></label>
+                  <input type="radio" name='rating1' id="rating4"></input>
+                  <label for="rating4" ></label>
+                  <input type="radio" name='rating1' id="rating5"></input>
+                  <label for="rating5" ></label>
+                  <br></br><br></br>
+                </div>
+              </div>
               <button className="btn item-button" type="button">View</button>
             </div>
           </div>
-          <div className="item-ins" id='item'>
-            <img className="item-image" src={courseImage} alt="" id='item-image'/>
-            <span className="item-title" id='item-title'>Mathematics</span>
-            <div className="item-details" id='item-details'>
-              <p id='grade'>Grade: 9 <br /> Instructor: James Wicked </p>
+
+          <div className="item-ins">
+            <img className="item-image-ins" src={science} alt="" />
+            <span className="item-title">Science</span>
+            <div className="item-details">
+              <p>Grade: 10 <br /> Instructor: Doris Wilson </p>
+              <p>BSc University of Moratuwa</p>
+              <div className='star-rating'>
+                <div class='rating'>
+                  <input type="radio" name='rating6' id="rating6"></input>
+                  <label for="rating6" ></label>
+                  <input type="radio" name='rating6' id="rating7"></input>
+                  <label for="rating7"></label>
+                  <input type="radio" name='rating6' id="rating8"></input>
+                  <label for="rating8"></label>
+                  <input type="radio" name='rating6' id="rating9"></input>
+                  <label for="rating9" ></label>
+                  <input type="radio" name='rating6' id="rating10"></input>
+                  <label for="rating10" ></label>
+                  <br></br><br></br>
+                </div>
+              </div>
               <button className="btn item-button" type="button">View</button>
             </div>
           </div>
-          <div className="item-ins" id='item'>
-            <img className="item-image" src={courseImage} alt="" id='item-image'/>
-            <span className="item-title" id='item-title'>Mathematics</span>
-            <div className="item-details" id='item-details'>
-              <p id='grade'>Grade: 9 <br /> Instructor: James Wicked </p>
+          <div className="item-ins">
+            <img className="item-image-ins" src={english} alt="" />
+            <span className="item-title">English</span>
+            <div className="item-details">
+              <p>Grade: 10 <br /> Instructor: Jennie Crigler </p>
+              <p>BSc University of Kelaniya</p>
+              <div className='star-rating'>
+                <div class='rating'>
+                  <input type="radio" name='rating11' id="rating11"></input>
+                  <label for="rating11" ></label>
+                  <input type="radio" name='rating11' id="rating12"></input>
+                  <label for="rating12"></label>
+                  <input type="radio" name='rating11' id="rating13"></input>
+                  <label for="rating13"></label>
+                  <input type="radio" name='rating11' id="rating14"></input>
+                  <label for="rating14" ></label>
+                  <input type="radio" name='rating11' id="rating15"></input>
+                  <label for="rating15" ></label>
+                  <br></br><br></br>
+                </div>
+              </div>
               <button className="btn item-button" type="button">View</button>
             </div>
           </div>
+          
+          {/* Repeat similar blocks for other courses */}
         </div>
       </section>
 
-      <div className="home-footer">
+      {/* <div className="home-footer">
         <ul>
           <li><a href="">Help desk</a></li>
           <li><a href="">Contact us</a></li>
@@ -79,7 +131,7 @@ const InstructorHome = () => {
         </ul>
         <hr />
         <p><center>© 2024, TutorMeister, Inc.</center></p>
-      </div>
+      </div> */}
     </>
   );
 };
