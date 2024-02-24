49bd464f5f0093a0b14de23744d321533b1ca199
DulamiA <119730845+DulamiA@users.noreply.github.com>
2024-02-24 16:25:36 +0530

    TM-47 - Create a course-UI

--- Views/src/pages/createCourse.js
+++ Views/src/pages/createCourse.js
@@ -31,35 +31,110 @@ const CreateCourse = () => {
         </nav>
       </header>
 
-      <div className="container">
-        <form action="" className="form create-form">
+      <div className="containterCreateCourse">
+        <form action="" className="formCreate">
+          <h3 className="heading">Create A Course</h3>
           <div className="row">
-            <h3 className="heading">Create A Course</h3>
-            <center><input type="text" className="inputbox details login-details" placeholder="Subject" /></center><br /><br />
-            <center><input type="text" className="inputbox details login-details" placeholder="Grade" /></center><br /><br />
-            <center><input type="email" className="inputbox details login-details" placeholder="Email" /></center><br /><br />
-            <center><input type="number" className="inputbox details login-details" placeholder="Phone Number" /></center><br /><br />
-            <center><textarea className="inputbox details login-details textArea" placeholder="Description" rows="4" cols="50"></textarea></center><br /><br />
-            <div className="flex buttons signup-btns">
-              <div>
-                <input type="button" value="Cancel" className="button signup-btn" onClick={() => window.location.href='instructorHome.html'} />
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Course name:</label>
               </div>
-              <div>
-                <input type="button" value="Continue to payment" className="button signup-btn" onClick={() => window.location.href=''} />
+              <div className="flexLabalCreate">
+                <input type="text" className="richText" id="courseName" placeholder="" />
               </div>
-            </div><br />
+            </div>
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Subject:</label>
+              </div>
+              <div className="flexLabalCreate">
+                <input type="text" className="richText" id="Subject" placeholder="" />
+              </div>
+            </div>
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Educational Level:</label>
+              </div>
+              <div className="flexLabalCreate">
+                <select id="educationalLevel" className="educational-qualification">
+                  <option value="" disabled selected>Choose an option</option>
+                  <option value="school">School</option>
+                  <option value="higherEducation">Higher Education</option>
+                  <option value="undergraduate">Undergraduate</option>
+                </select>
+              </div>
+            </div>
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Grade/Level:</label>
+              </div>
+              <div className="flexLabalCreate">
+                <input type="text" className="richText" id="Grade " placeholder="" />
+              </div>
+            </div>
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Teaching Style:</label>
+              </div>
+              <div className="flexLabalCreate">
+                <select id="teachingStyle" className="educational-qualification">
+                  <option value="" disabled selected>Choose an option</option>
+                  <option value="visual">Visual</option>
+                  <option value="auditory">Auditory</option>
+                  <option value="kinesthetic">Kinesthetic</option>
+                </select>
+              </div>
+            </div>
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Teaching Format:</label>
+              </div>
+              <div className="flexLabalCreate">
+                <select id="teachingFormat" className="educational-qualification">
+                  <option value="" disabled selected>Choose an option</option>
+                  <option value="oneOnOne">One-on-one</option>
+                  <option value="group">Group</option>
+                  <option value="both">Both</option>
+                </select>
+              </div>
+            </div>
+            <div className="flexPage2">
+              <div className="flexLabel flexCreate">
+                <label className="label-create-course">Teaching Mode:</label>
+              </div>
+              <div className="flexLabelCreateRadio">
+                <div class="optionGroup">
+                  <input type="radio" id="inPerson" name="learningMode" value="inPerson" className="radioButton" />
+                  <label for="inPerson">In person</label>
+                </div>
+                <div class="optionGroup">
+                  <input type="radio" id="online" name="learningMode" value="online" className="radioButton" />
+                  <label for="online">Online</label>
+                </div>
+              </div>
+            </div>
+            <div className="flex3Page2">
+              <div className="flexBox">
+                <input
+                  id='btnAddCourse'
+                  type="button"
+                  value="Add Course"
+                  className="button register-btn"
+                  onClick={() => window.location.href = '/instructor-home'}
+                />
+              </div>
+              <div className="flexBox">
+                <input
+                  id='btnCancel'
+                  type="button"
+                  value="Cancel"
+                  className="button signInstructor-btn"
+                  onClick={() => window.location.href = '/instructor-home'}
+                />
+              </div>
+            </div>
           </div>
         </form>
-      </div>
-
-      <div className="create-footer">
-        <ul>
-          <li><a href="">Help desk</a></li>
-          <li><a href="">Contact us</a></li>
-          <li><a href="">About us</a></li>
-        </ul>
-        <hr />
-        <p><center>© 2023, TutorMeister, Inc.</center></p>
       </div>
     </>
   );
