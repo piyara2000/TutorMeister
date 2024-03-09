889172bb9c6ba9e0b6798aebd94ccb7a7a0efe8e
piyara2000 <piyara.20212080@iit.ac.lk>
2024-03-10 03:19:52 +0530

    TM-75 - Adding session data and description to create course functionality

--- Controller/backend/create_course_controller.js
+++ Controller/backend/create_course_controller.js
@@ -6,16 +6,24 @@ const { check, validationResult } = require("express-validator");
 const session = require("express-session");
 
 exports.createCourse = (req, res) => {
+  if (!req.session.instructorId) {
+    return res.redirect("/login");
+  } else {
     var message = "";
     var userDetails = "";
     res.render("createCourse");
+  }
 };
 
 exports.createCoursePost = (req, res) => {
+  if (!req.session.instructorId) {
+    return res.redirect("/login");
+  } else {
     var message = "";
     const errors = validationResult(req);
     var post = req.body;
-  
+
+    var instructorId = req.session.instructorId;
     var courseName = post.courseName;
     courseName = courseName.trim();
     var subject = post.subject;
@@ -28,35 +36,45 @@ exports.createCoursePost = (req, res) => {
     var teachingMode = post.teachingMode;
     var video = post.video;
     video = video.trim();
-  
+    var desc = post.description;
+    desc = desc.trim();
+
     var courseid = 0;
     if (!errors.isEmpty()) {
       const validationMessage = errors.array();
-      res.render("createCourse", { message, userDetails: "", validationMessage });
+      res.render("createCourse", {
+        message,
+        userDetails: "",
+        validationMessage,
+      });
     } else {
-        var connection = db.getMySQLConnection();
-        connection.connect();
-        connection.query(
-            createCourseQuery.ADD_COURSE,
-            [
-            courseName,
-            subject,
-            eduLevel,
-            level,
-            teachingstyle,
-            teachingFormat,
-            teachingMode,
-            video,
-            ],
-            (err, rows) => {
-                if (err) {
-                    return res.send(err.stack);
-                } else {
-                    courseid = rows.insertId;
-                    connection.end();
-                    res.redirect("/instructor-home");
-                }
-            }
-        );
+      var connection = db.getMySQLConnection();
+      connection.connect();
+      connection.query(
+        createCourseQuery.ADD_COURSE,
+        [
+          courseName,
+          subject,
+          eduLevel,
+          level,
+          teachingstyle,
+          teachingFormat,
+          teachingMode,
+          video,
+          instructorId,
+          desc,
+        ],
+        (err, rows) => {
+          if (err) {
+            return res.send(err.stack);
+          } else {
+            courseid = rows.insertId;
+            req.session.courseId = courseid;
+            connection.end();
+            res.redirect("/instructor-home");
+          }
+        }
+      );
     }
+  }
 };
--- Controller/querymanager/createCourseQuery.js
+++ Controller/querymanager/createCourseQuery.js
@@ -1,6 +1,6 @@
 var ADD_COURSE =
-  "insert into tmdb.courseData (coursename, subject, eduLevel, level, teachingstyle, teachingformat, mode, video) values (?,?,?,?,?,?,?,?)";
+  "insert into tmdb.courseData (coursename, subject, eduLevel, level, teachingstyle, teachingformat, mode, video, instructorid, description) values (?,?,?,?,?,?,?,?,?,?)";
 
 module.exports = {
-  ADD_COURSE: ADD_COURSE
+  ADD_COURSE: ADD_COURSE,
 };
